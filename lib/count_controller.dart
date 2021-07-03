469fc10a318b5569469166754ef047df98dfb591
michelewtc <michelechan1996@gmail.com>
2021-07-03 00:55:31 -0300

    feat: CountController

---
+++ lib/count_controller.dart
@@ -0,0 +1,21 @@
+import 'package:mobx/mobx.dart';
+
+class CountController {
+  CountController() {
+    increment = Action(_increment);
+  }
+
+  //Variavel - count
+  final _count = Observable(0);
+  int get count => _count.value;
+  set count(int value) => _count.value = value;
+
+  //Action - increment
+  late Action increment;
+
+  void _increment() {
+    count++;
+  }
+
+  //Reaction - MOBX vai cuidar disso
+}
